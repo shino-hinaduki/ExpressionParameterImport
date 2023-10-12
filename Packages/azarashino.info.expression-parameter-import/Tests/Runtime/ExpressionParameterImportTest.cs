7291321dc2ce0dc3ab19b0cce449c8a0260624af
shino <shino.hinaduki@gmail.com>
2023-10-13 01:44:45 +0900

    test: fix warning

--- Packages/azarashino.info.expression-parameter-import/Tests/Runtime/ExpressionParameterImportTest.cs
+++ Packages/azarashino.info.expression-parameter-import/Tests/Runtime/ExpressionParameterImportTest.cs
@@ -10,13 +10,20 @@ namespace azarashino.info.expression_parameter_import.Tests.Runtime
 {
     public class ExpressionParameterImportTest
     {
+        internal class ExampleScriptableObject : ScriptableObject { }
+
         [Test]
         public void IsInsufficientTest()
         {
-            var dut = new ExpressionParameterImport();
-            Assert.That(dut.IsInsufficient, Is.True); // expression parameters not set
+            // create
+            var go = new GameObject();
+            go.AddComponent<ExpressionParameterImport>();
+            var dut = go.GetComponent<ExpressionParameterImport>();
 
-            dut.ExpressionParameters = new ScriptableObject();
+            // expression parameters not set
+            Assert.That(dut.IsInsufficient, Is.True);
+            // expression parameters set
+            dut.ExpressionParameters = ScriptableObject.CreateInstance<ExampleScriptableObject>();
             Assert.That(dut.IsInsufficient, Is.False);
         }
     }
