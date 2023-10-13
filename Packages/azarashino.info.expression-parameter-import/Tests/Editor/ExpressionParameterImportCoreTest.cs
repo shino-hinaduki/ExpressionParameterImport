0ee83f48523da59fb7e66c532b037b567d198eda
shino <shino.hinaduki@gmail.com>
2023-10-14 03:01:56 +0900

    test: 863069b で削除した関数対応

--- Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportCoreTest.cs
+++ Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportCoreTest.cs
@@ -16,53 +16,6 @@ namespace azarashino.info.expression_parameter_import.Tests.Editor
         internal class ExampleScriptableObject : ScriptableObject { }
 
         [Test]
-        public void GetParameterImportTargetsNullTest()
-        {
-            var menuCommand = new MenuCommand(null);
-            Assert.That(menuCommand.GetParameterImportTargets(), Is.Null);
-        }
-
-        [Test]
-        public void GetParameterImportTargetsOnlyExParamTest1()
-        {
-            var go = new GameObject();
-            go.AddComponent<ExpressionParameterImport>();
-
-            var menuCommand = new MenuCommand(go.GetComponent<ExpressionParameterImport>());
-            Assert.That(menuCommand.GetParameterImportTargets(), Is.Null);
-        }
-
-        [Test]
-        public void GetParameterImportTargetsOnlyExParamTest2()
-        {
-            var go = new GameObject();
-            go.AddComponent<ExpressionParameterImport>();
-            go.GetComponent<ExpressionParameterImport>().ExpressionParameters = ScriptableObject.CreateInstance<ExampleScriptableObject>();
-
-            var menuCommand = new MenuCommand(go.GetComponent<ExpressionParameterImport>());
-            Assert.That(menuCommand.GetParameterImportTargets(), Is.Null);
-        }
-
-
-        [Test]
-        public void GetParameterImportTargetsReadyTest()
-        {
-            var go = new GameObject();
-            go.AddComponent<ExpressionParameterImport>();
-            go.GetComponent<ExpressionParameterImport>().ExpressionParameters = ScriptableObject.CreateInstance<ExampleScriptableObject>();
-            go.AddComponent<ModularAvatarParameters>();
-
-            var menuCommand = new MenuCommand(go.GetComponent<ExpressionParameterImport>());
-            var dst = menuCommand.GetParameterImportTargets();
-            Assert.That(dst, Is.Not.Null);
-
-            var (gameObject, srcParam, maParam) = dst.Value;
-            Assert.That(gameObject, Is.EqualTo(go));
-            Assert.That(srcParam, Is.EqualTo(go.GetComponent<ExpressionParameterImport>()));
-            Assert.That(maParam, Is.EqualTo(go.GetComponent<ModularAvatarParameters>()));
-        }
-
-        [Test]
         public void MakeBackupTest()
         {
             var srcGameObject = new GameObject();
