d2096f9281a5bb4d73cfcd2d1714a68a0f2f5711
shino <shino.hinaduki@gmail.com>
2023-10-14 03:33:10 +0900

    test: (wip) ImportFrom 実装中

--- Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportCoreTest.cs
+++ Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportCoreTest.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEditor;
 
+using ExpressionParameters = VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionParameters;
 using nadena.dev.modular_avatar.core;
 using azarashino.info.expression_parameter_import.Runtime;
 using azarashino.info.expression_parameter_import.Editor;
@@ -13,7 +15,10 @@ namespace azarashino.info.expression_parameter_import.Tests.Editor
 {
     public class ExpressionParameterImportCoreTest
     {
-        internal class ExampleScriptableObject : ScriptableObject { }
+        internal class ExampleScriptableObject : ScriptableObject
+        {
+
+        }
 
         [Test]
         public void MakeBackupTest()
@@ -27,6 +32,55 @@ namespace azarashino.info.expression_parameter_import.Tests.Editor
             Assert.That(dstGameObject, Is.Not.EqualTo(srcGameObject));
             Assert.That(dstGameObject.name, Is.EqualTo($"MakeBackUpTest_{srcGameObject.name}"));
             Assert.That(dstGameObject.activeSelf, Is.False);
+        }
+
+        public class ImportFromTestData
+        {
+            public ModularAvatarParameters SrcMaParam { get; set; }
+            public ExpressionParameterImport SrcExParam { get; set; }
+
+            public ModularAvatarParameters ExpectMaParam { get; set; }
+
+            public bool IsOk(ModularAvatarParameters actMaParam) => Enumerable.SequenceEqual(actMaParam.parameters, ExpectMaParam.parameters);
+        }
+        public static IEnumerable<ImportFromTestData> ImportFromTestDatas
+        {
+            get
+            {
+                yield return new ImportFromTestData()
+                {
+                    SrcMaParam = new ModularAvatarParameters()
+                    {
+                        parameters = new List<ParameterConfig>() {
+                            new ParameterConfig() {
+                            },
+                        }
+                    },
+                    SrcExParam = new ExpressionParameterImport()
+                    {
+                        Storategy = ImportStrategy.ApplyAll,
+                        ExpressionParameters = new ExpressionParameters()
+                        {
+                            parameters = new ExpressionParameters.Parameter[] {
+                            }
+                        }
+                    },
+                    ExpectMaParam = new ModularAvatarParameters()
+                    {
+                        parameters = new List<ParameterConfig>() {
+                            new ParameterConfig() {
+                            },
+                        }
+                    },
+
+                };
+            }
+        }
+
+        [TestCaseSource(nameof(ImportFromTestData))]
+        public void ImportFromTest(ImportFromTestData testData)
+        {
+            Assert.That(testData.IsOk(testData.SrcMaParam.ImportFrom(testData.SrcExParam)), Is.True);
         }
 
     }
