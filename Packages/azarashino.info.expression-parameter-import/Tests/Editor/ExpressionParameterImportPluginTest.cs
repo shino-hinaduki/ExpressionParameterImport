56f200b72a3ba36fe3fbf76595bbf73e008bbab8
shino <shino.hinaduki@gmail.com>
2023-10-14 22:23:17 +0900

    test: テンプレートの項目を削除

--- Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportPluginTest.cs
+++ Packages/azarashino.info.expression-parameter-import/Tests/Editor/ExpressionParameterImportPluginTest.cs
@@ -8,21 +8,5 @@ namespace azarashino.info.expression_parameter_import.Tests.Editor
 {
     public class ExpressionParameterImportPluginTest
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void ExpressionParameterImportPluginTestSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator ExpressionParameterImportPluginTestWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
     }
 }
